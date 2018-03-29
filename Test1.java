71639319e7aedbcbeb5f4ed97cca4ca287e4093c
sai mahesh <rsmahesh12@gmail.com>
2018-03-29 12:44:14 +0530

    object creation example

---
+++ Test1.java
@@ -0,0 +1,17 @@
+class Test1{
+int i=10;
+int j=20;
+
+[Apublic void print(){
+System.out.println("i am practicing GIT");
+}
+
+public void add(){
+int z=i+j;
+System.out.println(z);
+public static void main(String [] args){
+Test1 t1-new Test1();
+[A[C[C[C[C[C[C[C[t1.print();
+t1.add();
+}
+}
